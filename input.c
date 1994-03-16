c00633078b953a440e9fa6f45d09e15f7bc7986f
Michael K. Johnson <johnsonm@danlj.org>
1994-03-16 20:12:06 +0000

    Now almost working.  Need to get signals straightened out for second process.

--- input.c
+++ input.c
@@ -11,7 +11,10 @@
  */
 
 /* RCS log:
- * $Log$
+ * $Log: input.c,v $
+ * Revision 1.1  1994/03/15  18:27:33  johnsonm
+ * Initial revision
+ *
  *
  */
 
@@ -19,31 +22,52 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <pwd.h>
 #include "vlock.h"
 
 
-static char rcsid[] = "$Id: input.c,v 1.1 1994/03/15 18:27:33 johnsonm Exp $";
+static char rcsid[] = "$Id: input.c,v 1.2 1994/03/16 20:12:06 johnsonm Exp $";
 
 
 /* get_password() should fork a new process to get the password. */
 /* get_password() should return right away, but the new process  */
 /* should not exit until it gets a proper password.              */
 
+/* size of input buffer.  Shouldn't probably be very big.  Try 1... */
+#define INBUFSIZE 1
+
 void get_password(void) {
 
   pid_t pid;
+  static char inbuf[INBUFSIZE];
 
   if ((pid = fork()) < 0) {
-    restore_terminal();
     perror("fork failed");
     exit(1);
-  } else
-    if (pid > 0)
+  } else {
+    if (pid > 0) {
       /* Parent */
       return; /* to wait for signals, including SIGCHLD from this process... */
-    else
+    } else {
       /* Child */
-      getchar(); /* should really get password here... */
+      /* we need to ignore SIGCHLD now... */
+      ignore_sigchld();
+      printf("Please enter the password to unlock.\n");
+      printf("%s's password:", getpwuid(getuid())->pw_name);
+      fflush(stdout);
+
+      /* set the terminal characteristics to not echo, etc... */
+      set_terminal();
+
+      /* Should get password here... */
+      read(STDIN_FILENO, &inbuf, INBUFSIZE);
+
+      /* reset the terminal characteristics to echo again before exiting */
+      restore_terminal();
+    }
+  }
+  fprintf(stderr, "exiting second process\n");
+  fflush(stderr);
   exit(0);
 
 }
