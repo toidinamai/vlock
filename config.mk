72a243ea6718032618c15948c373c1253d477ac5
Frank Benkstein <frank@benkstein.net>
2007-05-09 21:11:11 +0200

    add option for building without root pass prompt

--- config.mk
+++ config.mk
@@ -1,10 +1,15 @@
 # should we build against pam?
 USE_PAM = y
+USE_ROOT_PASS = y
 PREFIX = /usr/local
 
 CC = gcc
 CFLAGS = -O2
 INSTALL = install
+
+ifneq ($(USE_ROOT_PASS),y)
+	CFLAGS += -DNO_ROOT_PASS
+endif
 
 ifeq ($(USE_PAM),y)
 	CFLAGS += -DUSE_PAM
