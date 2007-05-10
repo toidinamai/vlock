c01e4dfd0656885ed2254adda856d3b592d0a092
Frank Benkstein <frank-vlock@benkstein.net>
2007-05-10 12:06:51 +0200

    GNU public license -> GNU General Public License

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 /* version.h -- version header file for vlock
  *
  * This program is copyright (C) 1994 Michael K. Johnson, and is free
- * software, which is freely distributable under the terms of the
- * GNU public license, included as the file COPYING in this
+ * software which is freely distributable under the terms of the
+ * GNU General Public License version 2, included as the file COPYING in this
  * distribution.  It is NOT public domain software, and any
  * redistribution not permitted by the GNU General Public License is
  * expressly forbidden without prior written permission from
@@ -10,4 +10,4 @@
  *
  */
 
-#define VERSION "vlock version 1.3\n"
+#define VERSION "vlock version 1.4-rc2\n"
