bf454897a285493698f64a1d7b528069071079a4
Frank Benkstein <frank-vlock@benkstein.net>
2007-05-10 12:06:51 +0200

    GNU public license -> GNU General Public License

--- help.c
+++ help.c
@@ -1,8 +1,8 @@
 /* help.c -- print help and usage message.
  *
  * This program is copyright (C) 1994 Michael K. Johnson, and is free
- * software, which is freely distributable under the terms of the
- * GNU public license version 2, included as the file COPYING in this
+ * software which is freely distributable under the terms of the
+ * GNU General Public License version 2, included as the file COPYING in this
  * distribution.  It is NOT public domain software, and any
  * redistribution not permitted by the GNU General Public License is
  * expressly forbidden without prior written permission from
