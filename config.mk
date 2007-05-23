d10dd57b6f798310c2f28edfe0a92c09eaf051a8
Frank Benkstein <frank-vlock@benkstein.net>
2007-05-23 10:31:14 +0200

    remove non-PAM support from Makefile

--- config.mk
+++ config.mk
@@ -6,16 +6,10 @@ DESTDIR =
 
 CC = gcc
 CFLAGS = -O2
+LDFLAGS = -ldl -lpam -lpam_misc
+
 INSTALL = install
 
 ifneq ($(USE_ROOT_PASS),y)
 	CFLAGS += -DNO_ROOT_PASS
-endif
-
-ifeq ($(USE_PAM),y)
-	CFLAGS += -DUSE_PAM
-	LDFLAGS += -ldl -lpam -lpam_misc
-else
-	CFLAGS += -DSHADOW_PWD
-	LDFLAGS += -lcrypt
 endif
