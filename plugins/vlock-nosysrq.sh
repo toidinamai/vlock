d7cc0283b35958ea2dfb5a0d50df6682fddab5c0
Frank Benkstein <frank-vlock@benkstein.net>
2007-08-17 19:31:40 +0200

    src/vlock-nosysrq.sh: fix help message

--- plugins/vlock-nosysrq.sh
+++ plugins/vlock-nosysrq.sh
@@ -1,5 +1,5 @@
 short_option="s"
 long_option="disable-sysrq"
 help="\
--s or --disable-sysrq: disable sysrq while consoles are locked to
-       prevent killing vlock with SAK, requires --all."
+-s or --disable-sysrq: disable SysRq while consoles are locked to
+       prevent killing vlock with SAK, implies --all."
