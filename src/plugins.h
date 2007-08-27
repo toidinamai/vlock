79a9b5d3e48928adad08480cf6b91853fb2da601
Frank Benkstein <frank-vlock@benkstein.net>
2007-08-27 22:19:10 +0200

    src/plugins.h: add copyright header

--- src/plugins.h
+++ src/plugins.h
@@ -1,3 +1,16 @@
+/* plugins.h -- plugins header file for vlock,
+ *              the VT locking program for linux
+ *
+ * This program is copyright (C) 2007 Frank Benkstein, and is free
+ * software which is freely distributable under the terms of the
+ * GNU General Public License version 2, included as the file COPYING in this
+ * distribution.  It is NOT public domain software, and any
+ * redistribution not permitted by the GNU General Public License is
+ * expressly forbidden without prior written permission from
+ * the author.
+ *
+ */
+
 int load_plugin(const char *name, const char *plugin_dir);
 void unload_plugins(void);
 int resolve_dependencies(void);
