c990e8ba5e8b37fa9670b2ab357aff097dd1e355
Frank Benkstein <frank-vlock@benkstein.net>
2007-08-27 23:40:41 +0200

    plugins/vlock-blank.c: add dependency: vlock-all

--- plugins/vlock-blank.c
+++ plugins/vlock-blank.c
@@ -5,6 +5,8 @@
 
 #include "vlock_plugin.h"
 
+const char *depends[] = { "vlock-all", NULL };
+
 int vlock_save(void __attribute__((__unused__)) **ctx) {
   char arg[] = {TIOCL_BLANKSCREEN, 0};
   return ioctl(STDIN_FILENO, TIOCLINUX, arg);
