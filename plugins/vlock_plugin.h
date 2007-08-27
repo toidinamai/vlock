fba61e3de37825a3493a3e879f7a0a2a356c2a6e
Frank Benkstein <frank-vlock@benkstein.net>
2007-08-28 00:12:32 +0200

    plugins/vlock_plugin.h: add copyright header

--- plugins/vlock_plugin.h
+++ plugins/vlock_plugin.h
@@ -1,3 +1,16 @@
+/* vlock_plugin.h -- header file for plugins for vlock,
+ *                   the VT locking program for linux
+ *
+ * This program is copyright (C) 2007 Frank Benkstein, and is free
+ * software which is freely distributable under the terms of the
+ * GNU General Public License version 2, included as the file COPYING in this
+ * distribution.  It is NOT public domain software, and any
+ * redistribution not permitted by the GNU General Public License is
+ * expressly forbidden without prior written permission from
+ * the author.
+ *
+ */
+
 /* A plugin might define these arrays to indicate
  * that its must always be called before or after
  * the ones listed in the respective arrays.  If
