b05c4a0128dbeabe230539d5d0c913145c9c7223
Frank Benkstein <frank-vlock@benkstein.net>
2007-08-28 17:05:38 +0200

    src/list.c, src/list.h: add copyright header

--- src/list.h
+++ src/list.h
@@ -1,3 +1,16 @@
+/* list.h -- doubly linked list header file for vlock,
+ *           the VT locking program for linux
+ *
+ * This program is copyright (C) 2007 Frank Benkstein, and is free
+ * software which is freely distributable under the terms of the
+ * GNU General Public License version 2, included as the file COPYING in this
+ * distribution.  It is NOT public domain software, and any
+ * redistribution not permitted by the GNU General Public License is
+ * expressly forbidden without prior written permission from
+ * the author.
+ *
+ */
+
 struct List {
   void *data;
   struct List *next;
