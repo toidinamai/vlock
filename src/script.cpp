a13b74e5c14eae4c3496626c0310ceecfd74ca77
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-12 23:28:26 +0200

    src/script.{cpp,h}: broken stub implementation of dependency getting

--- src/script.cpp
+++ src/script.cpp
@@ -1,18 +1,161 @@
+#include <unistd.h>
+#include <limits.h>
+#include <sys/resource.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "script.h"
 
 /* hard coded paths */
 #define VLOCK_SCRIPT_DIR PREFIX "/lib/vlock/scripts"
 
+static void get_dependency(const char *path, const char *name, list<string> dependency);
+static pid_t launch_script(const char *path, int pipe_fd);
+
 Script::Script(string name) : Plugin(name)
 {
   char *path;
+  int pipe_fds[2];
 
   /* format the plugin path */
   if (asprintf(&path, "%s/%s", VLOCK_SCRIPT_DIR, name.c_str()) < 0)
     throw std::bad_alloc();
+
+  /* load dependencies */
+  for (vector<string>::iterator it = dependency_names.begin();
+      it != dependency_names.end(); it++)
+      get_dependency(path, (*it).c_str(), dependencies[*it]);
+
+  if (pipe(pipe_fds) < 0)
+    throw PluginException("pipe() failed");
+
+  fd = pipe_fds[1];
+
+  pid = launch_script(path, pipe_fds[0]);
+
+  free(path);
+
+  throw PluginException("No way!");
+}
+
+Script::~Script()
+{
+  close(fd);
+  // XXX: waitpid, kill(pid, SIGTERM), waitpdid, kill(pid, SIGKILL)
 }
 
 bool Script::call_hook(string name)
 {
+  (void) name;
   return true;
+}
+
+bool close_all_fds(void)
+{
+  rlimit r;
+
+  if (getrlimit(RLIMIT_NOFILE, &r) < 0)
+    return false;
+
+  // close all file descriptors
+  for (unsigned int i = 0; i < r.rlim_cur; i++) {
+    switch (i) {
+      case STDIN_FILENO:
+      case STDOUT_FILENO:
+      case STDERR_FILENO:
+        break;
+      default:
+        close(i);
+    }
+  }
+
+  return true;
+}
+
+static void get_dependency(const char *path, const char *name, list<string> dependency)
+{
+  int pipe_fds[2];
+  pid_t pid = fork();
+  timeval timeout;
+  string data;
+
+  if (pipe(pipe_fds) < 0)
+    throw PluginException("pipe() failed");
+
+  if (pid == 0) {
+    int nullfd = open("/dev/null", O_RDWR);
+
+    if (nullfd < 0)
+      _exit(1);
+
+    // redirect stdio
+    (void) dup2(nullfd, STDIN_FILENO);
+    (void) dup2(pipe_fds[1], STDOUT_FILENO);
+    (void) dup2(nullfd, STDERR_FILENO);
+
+    // close all other file descriptors
+    if (!close_all_fds())
+      _exit(1);
+
+    // drop privileges
+    setgid(getgid());
+    setuid(getuid());
+
+    (void) execl(path, path, name, NULL);
+
+    _exit(1);
+  }
+
+  (void) close(pipe_fds[1]);
+
+  if (pid < 0)
+    goto err;
+
+  // if (fcntl(pipe_fds[0], F_SETFD, O_NONBLOCK) < 0)
+  //   goto err;
+
+  for (;;) {
+    char buffer[LINE_MAX];
+    ssize_t len;
+
+    fd_set read_fds;
+
+    FD_ZERO(&read_fds);
+    FD_SET(pipe_fds[0], &read_fds);
+
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+
+    if (select(pipe_fds[0]+1, &read_fds, NULL, NULL, &timeout) != 1)
+      goto err;
+
+    if ((len = read(pipe_fds[0], buffer, sizeof buffer - 1)) < 0)
+      goto err;
+
+    buffer[len] = '\0';
+    data += buffer;
+
+    if (data.length() > LINE_MAX)
+      goto err;
+  }
+
+  for (size_t pos1 = 0, pos2 = data.find('\n', pos1);
+      pos1 < data.length();
+      pos1 = pos2+1, pos2 = data.find('\n', pos1))
+    if ((pos2 - pos1) > 1) {
+      fprintf(stderr, "found dependency '%s'\n", data.substr(pos1, pos2-1).c_str());
+      dependency.push_back(data.substr(pos1, pos2-1));
+    }
+
+err:
+    (void) close(pipe_fds[1]);
+    throw PluginException("getting dependencies failed");
+}
+
+static pid_t launch_script(const char *path, int pipe_fd)
+{
+  (void) path;
+  return 0;
 }
--- src/script.h
+++ src/script.h
@@ -1,10 +1,18 @@
+#include <unistd.h>
 #include <string>
 #include "plugin.h"
 
 class Script : public Plugin
 {
+  // file descriptor for the child
+  int fd;
+
+  // pid of the child
+  pid_t pid;
+
 public:
   Script(string name);
+  ~Script();
 
   bool call_hook(string name);
 };
