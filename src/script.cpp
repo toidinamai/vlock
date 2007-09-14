01ce394b0b737d181cf2b1351bd7093d5c1cdbbb
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-14 13:59:33 +0200

    src/script.cpp: comments and small fixes

--- src/script.cpp
+++ src/script.cpp
@@ -15,28 +15,46 @@
 /* hard coded paths */
 #define VLOCK_SCRIPT_DIR PREFIX "/lib/vlock/scripts"
 
+// Load the named dependency into the given list from the script at the given path.
+// Throws PluginException on error.
 static void get_dependency(const char *path, const char *name, list<string>& dependency);
+
+// Launch the script with stdin redirected from pipe_fd.  Returns the PID of
+// the script or throw PluginException on error.  In any case, pipe_fd is
+// closed before this function returns.
 static pid_t launch_script(const char *path, int pipe_fd);
+
+// Wait the specified amount of time for the child with the given PID to terminate.
+// Returns true if the child did terminate and false otherwise.
 static bool wait_for_death(pid_t pid, long sec, long usec);
+
+// Kill the child with the given PID by sending SIGTERM first and SIGKILL shortly
+// afterwards.
 static void ensure_death(pid_t pid);
+
+// Close all file descriptors that may be open except STDIN_FILENO,
+// STDOUT_FILENO, and STDERR_FILENO.
+static void close_all_fds(void);
 
 Script::Script(string name) : Plugin(name)
 {
   char path[FILENAME_MAX];
   int pipe_fds[2];
 
-  /* format the plugin path */
+  // format the plugin path
   if (snprintf(path, sizeof path, "%s/%s", VLOCK_SCRIPT_DIR, name.c_str()) > (ssize_t)sizeof path)
     throw PluginException("plugin '" + name + "' filename too long");
 
+  // test if the script can be accessed
   if (access(path, R_OK | X_OK) != 0)
     throw PluginException(string(path) + ": " + strerror(errno));
 
-  /* load dependencies */
+  // load dependencies
   for (vector<string>::iterator it = dependency_names.begin();
       it != dependency_names.end(); it++)
       get_dependency(path, (*it).c_str(), dependencies[*it]);
 
+  // open a pipe for the script
   if (pipe(pipe_fds) < 0)
     throw PluginException("pipe() failed");
 
@@ -45,6 +63,7 @@ Script::Script(string name) : Plugin(name)
   // set non-blocking mode
   (void) fcntl(fd, F_SETFL, O_NONBLOCK);
 
+  // launch the script
   pid = launch_script(path, pipe_fds[0]);
 }
 
@@ -59,8 +78,7 @@ void Script::call_hook(string name)
 {
   ssize_t length = name.length() + 1;
   ssize_t wlength;
-  struct sigaction act;
-  struct sigaction oldact;
+  struct sigaction act, oldact;
 
   // ignore SIGPIPE
   (void) sigemptyset(&(act.sa_mask));
@@ -68,6 +86,7 @@ void Script::call_hook(string name)
   act.sa_handler = SIG_IGN;
   (void) sigaction(SIGPIPE, &act, &oldact);
 
+  // send hook name and line feed through the pipe
   wlength = write(fd, (name + "\n").data(), length);
 
   // restore SIGPIPE handler
@@ -77,26 +96,30 @@ void Script::call_hook(string name)
     throw PluginException("error calling hook '" + name + "' for script '" + this->name + "'");
 }
 
-bool close_all_fds(void)
+static void close_all_fds(void)
 {
-  rlimit r;
+  struct rlimit r;
+  int maxfd;
 
-  if (getrlimit(RLIMIT_NOFILE, &r) < 0)
-    return false;
+  // get the maximum number of file descriptors
+  if (getrlimit(RLIMIT_NOFILE, &r) == 0)
+    maxfd = r.rlim_cur;
+  else
+    // hopefully safe default
+    maxfd = 1024;
 
-  // close all file descriptors except stdio
-  for (unsigned int i = 0; i < r.rlim_cur; i++) {
+  // close all file descriptors except STDIN_FILENO, STDOUT_FILENO and
+  // STDERR_FILENO
+  for (int i = 0; i < maxfd; i++) {
     switch (i) {
       case STDIN_FILENO:
       case STDOUT_FILENO:
       case STDERR_FILENO:
         break;
       default:
-        close(i);
+        (void) close(i);
     }
   }
-
-  return true;
 }
 
 static void get_dependency(const char *path, const char *name, list<string>& dependency)
@@ -107,6 +130,7 @@ static void get_dependency(const char *path, const char *name, list<string>& dep
   string data;
   string errmsg;
 
+  // open a pipe for the script
   if (pipe(pipe_fds) < 0)
     throw PluginException("pipe() failed");
 
@@ -125,8 +149,7 @@ static void get_dependency(const char *path, const char *name, list<string>& dep
     (void) dup2(nullfd, STDERR_FILENO);
 
     // close all other file descriptors
-    if (!close_all_fds())
-      _exit(1);
+    close_all_fds();
 
     // drop privileges
     setgid(getgid());
@@ -206,8 +229,7 @@ static pid_t launch_script(const char *path, int pipe_fd)
     (void) dup2(nullfd, STDERR_FILENO);
 
     // close all other file descriptors
-    if (!close_all_fds())
-      _exit(1);
+    close_all_fds();
 
     // drop privileges
     setgid(getgid());
@@ -234,56 +256,56 @@ static void handle_alarm(int __attribute__((unused)) signum)
 static bool wait_for_death(pid_t pid, long sec, long usec)
 {
   int status;
+  struct sigaction act, oldact;
+  struct itimerval timer, otimer;
+  bool result;
 
-  if (sec > 0 || usec > 0) {
-    struct sigaction act, oldact;
-    struct itimerval timer, otimer;
-    bool result;
+  // ignore SIGALRM
+  sigemptyset(&act.sa_mask);
+  act.sa_handler = handle_alarm;
+  act.sa_flags = 0;
+  sigaction(SIGALRM, &act, &oldact);
 
-    // ignore SIGALRM
-    sigemptyset(&act.sa_mask);
-    act.sa_handler = handle_alarm;
-    act.sa_flags = 0;
-    sigaction(SIGALRM, &act, &oldact);
+  // initialize timer
+  timer.it_interval.tv_sec = 0;
+  timer.it_interval.tv_usec = 0;
+  timer.it_value.tv_sec = 1;
+  timer.it_value.tv_usec = 500000L;
 
-    // initialize timer
-    timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 0;
-    timer.it_value.tv_sec = 1;
-    timer.it_value.tv_usec = 500000L;
+  // set timer
+  setitimer(ITIMER_REAL, &timer, &otimer);
 
-    // set timer
-    setitimer(ITIMER_REAL, &timer, &otimer);
+  // wait until interupted by timer
+  result = (waitpid(pid, &status, 0) == pid);
 
-    // wait until interupted by timer
-    result = (waitpid(pid, &status, 0) == pid);
+  // restore signal handler
+  sigaction(SIGALRM, &oldact, NULL);
 
-    // restore signal handler
-    sigaction(SIGALRM, &oldact, NULL);
+  // restore timer
+  setitimer(ITIMER_REAL, &otimer, NULL);
 
-    // restore timer
-    setitimer(ITIMER_REAL, &otimer, NULL);
-
-    return result;
-  } else {
-    return (waitpid(pid, &status, WNOHANG) == pid);
-  }
+  return result;
 }
 
 static void ensure_death(pid_t pid)
 {
   int status;
 
+  // look if already dead
   if (waitpid(pid, &status, WNOHANG) == pid)
     return;
 
+  // kill!!!
   (void) kill(pid, SIGTERM);
 
+  // wait 500ms for death
   if (wait_for_death(pid, 0, 500000L))
     return;
 
+  // kill harder!!!
   (void) kill(pid, SIGKILL);
   (void) kill(pid, SIGCONT);
 
+  // wait until dead
   (void) waitpid(pid, &status, 0);
 }
