c89a71d3b8db4d2aba243c6293e5e925f46b3302
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-15 23:43:21 +0200

    src/module.c: dlopen()

--- src/module.c
+++ src/module.c
@@ -7,34 +7,56 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <dlfcn.h>
 
 #include <sys/types.h>
 
 #include "plugin.h"
+#include "util.h"
 
 #include "module.h"
 
 #define VLOCK_MODULE_DIR PREFIX "/lib/vlock/modules"
 
+struct module_context
+{
+  void *dl_handle;
+};
+
 struct plugin *open_module(const char *name, char **error)
 {
   char *path;
   struct plugin *m = __allocate_plugin(name);
+  struct module_context *context = ensure_malloc(sizeof (struct module_context));
 
   if (asprintf(&path, "%s/%s.so", VLOCK_MODULE_DIR, name) < 0) {
-    __destroy_plugin(m);
     *error = strdup("filename too long");
+    free(context);
+    __destroy_plugin(m);
     return NULL;
   }
 
   if (access(path, R_OK) < 0) {
     (void) asprintf(error, "%s: %s", path, strerror(errno));
     free(path);
+    free(context);
+    __destroy_plugin(m);
+    return NULL;
+  }
+
+  context->dl_handle = dlopen(path, RTLD_NOW | RTLD_LOCAL);
+
+  if (context->dl_handle == NULL) {
+    *error = strdup(dlerror());
+    free(path);
+    free(context);
     __destroy_plugin(m);
     return NULL;
   }
 
   free(path);
+
+  m->context = context;
 
   return m;
 }
