6cd3f556464edec0af32845d73e7c4f4830823e7
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-15 16:39:43 +0200

    console_switch stubs

--- src/console_switch.c
+++ src/console_switch.c
@@ -1,3 +1,12 @@
 #include "console_switch.h"
 
 bool console_switch_locked = false;
+
+void lock_console_switch(void)
+{
+  console_switch_locked = true;
+}
+
+void unlock_console_switch(void)
+{
+}
--- src/console_switch.h
+++ src/console_switch.h
@@ -1,3 +1,6 @@
 #include <stdbool.h>
 
 extern bool console_switch_locked;
+
+void lock_console_switch(void);
+void unlock_console_switch(void);
