8f430d697293f23bd36e9b133c1f0da698c503b4
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-15 23:33:57 +0200

    src/module.c: halfway through open_module

--- src/module.c
+++ src/module.c
@@ -1,8 +1,40 @@
-#include <stddef.h>
+#if !defined(__FreeBSD__) && !defined(_GNU_SOURCE)
+#define _GNU_SOURCE
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/types.h>
+
+#include "plugin.h"
 
 #include "module.h"
 
+#define VLOCK_MODULE_DIR PREFIX "/lib/vlock/modules"
+
 struct plugin *open_module(const char *name, char **error)
 {
-  return NULL;
+  char *path;
+  struct plugin *m = __allocate_plugin(name);
+
+  if (asprintf(&path, "%s/%s.so", VLOCK_MODULE_DIR, name) < 0) {
+    __destroy_plugin(m);
+    *error = strdup("filename too long");
+    return NULL;
+  }
+
+  if (access(path, R_OK) < 0) {
+    (void) asprintf(error, "%s: %s", path, strerror(errno));
+    free(path);
+    __destroy_plugin(m);
+    return NULL;
+  }
+
+  free(path);
+
+  return m;
 }
