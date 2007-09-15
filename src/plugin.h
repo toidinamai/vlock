e547f2b801c76ac76f85826d44ae49971f0911a6
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-15 14:12:19 +0200

    src/plugin.h: more design code

--- src/plugin.h
+++ src/plugin.h
@@ -1,6 +1,29 @@
+#include <stdbool.h>
+
+#define NR_DEPENDENCIES 6
+#define NR_HOOKS 4
+
+struct list;
+
+// array of dependency names
+extern const char *dependency_names[NR_DEPENDENCIES];
+
+// array of hook names, terminated by NULL
+extern const char *hook_names[NR_HOOKS];
+
 struct plugin
 {
   const char *name;
+
+  // dependencies
+  struct list *after;
+  struct list *before;
+  struct list *requires;
+  struct list *needs;
+  struct list *depends;
+  struct list *conflicts;
+
+  bool (*call_hook)(struct plugin *p, const char *name);
 };
 
 struct plugin *allocate_plugin(const char *name);
