0d18c52899cda4010cc7f34bc4e95cbadf2e27d0
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-15 17:04:49 +0200

    move code from all plugin to console_switch

--- modules/all.c
+++ modules/all.c
@@ -20,96 +20,17 @@
 #include <errno.h>
 #include <signal.h>
 
-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-#include <sys/consio.h>
-#else
-#include <sys/vt.h>
-#endif
-
 #include "vlock_plugin.h"
 #include "console_switch.h"
 
-/* This handler is called by a signal whenever a user tries to
- * switch away from this virtual console. */
-static void release_vt(int __attribute__ ((__unused__)) signum)
+bool vlock_start(void __attribute__((unused)) **ctx_ptr)
 {
-  /* Deny console switch. */
-  (void) ioctl(STDIN_FILENO, VT_RELDISP, 0);
-}
-
-/* This handler is called whenever a user switches to this
- * virtual console. */
-static void acquire_vt(int __attribute__ ((__unused__)) signum)
-{
-  /* Acknowledge console switch. */
-  (void) ioctl(STDIN_FILENO, VT_RELDISP, VT_ACKACQ);
-}
-
-bool vlock_start(void **ctx_ptr)
-{
-  struct vt_mode vtm, *ctx;
-  struct sigaction sa;
-
-  /* Allocate the context. */
-  if ((ctx = malloc(sizeof *ctx)) == NULL)
-    return false;
-
-  /* Get the virtual console mode. */
-  if (ioctl(STDIN_FILENO, VT_GETMODE, &vtm) < 0) {
-    if (errno == ENOTTY || errno == EINVAL)
-      fprintf(stderr, "vlock-all: this terminal is not a virtual console\n");
-    else
-      perror("vlock-all: could not get virtual console mode");
-
-    goto err;
-  }
-
-  /* Save a copy of the virtual console mode. */
-  (void) memcpy(ctx, &vtm, sizeof vtm);
-
-  (void) sigemptyset(&(sa.sa_mask));
-  sa.sa_flags = SA_RESTART;
-  sa.sa_handler = release_vt;
-  (void) sigaction(SIGUSR1, &sa, NULL);
-  sa.sa_handler = acquire_vt;
-  (void) sigaction(SIGUSR2, &sa, NULL);
-
-  /* set terminal switching to be process governed */
-  vtm.mode = VT_PROCESS;
-  /* set terminal release signal, i.e. sent when switching away */
-  vtm.relsig = SIGUSR1;
-  /* set terminal acquire signal, i.e. sent when switching here */
-  vtm.acqsig = SIGUSR2;
-  /* set terminal free signal, not implemented on either FreeBSD or Linux */
-  /* Linux ignores it but FreeBSD wants a valid signal number here */
-  vtm.frsig = SIGHUP;
-
-  /* set virtual console mode to be process governed
-   * thus disabling console switching */
-  if (ioctl(STDIN_FILENO, VT_SETMODE, &vtm) < 0) {
-    perror("vlock-all: could not set virtual console mode");
-    goto err;
-  }
-
-  *ctx_ptr = ctx;
-  return true;
-
-err:
-  free(ctx);
-  return false;
+  lock_console_switch();
+  return console_switch_locked;
 }
 
 bool vlock_end(void **ctx_ptr)
 {
-  struct vt_mode *ctx = *ctx_ptr;
-
-  if (ctx != NULL) {
-    /* globally enable virtual console switching */
-    if (ioctl(STDIN_FILENO, VT_SETMODE, ctx) < 0) {
-      perror("vlock-all: could not restore console mode");
-      return false;
-    }
-  }
-
+  unlock_console_switch();
   return true;
 }
--- src/console_switch.c
+++ src/console_switch.c
@@ -1,12 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <signal.h>
+
+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+#include <sys/consio.h>
+#else
+#include <sys/vt.h>
+#endif
+
 #include "console_switch.h"
 
 bool console_switch_locked = false;
 
+/* This handler is called by a signal whenever a user tries to
+ * switch away from this virtual console. */
+static void release_vt(int __attribute__ ((__unused__)) signum)
+{
+  /* Deny console switch. */
+  (void) ioctl(STDIN_FILENO, VT_RELDISP, 0);
+}
+
+/* This handler is called whenever a user switches to this
+ * virtual console. */
+static void acquire_vt(int __attribute__ ((__unused__)) signum)
+{
+  /* Acknowledge console switch. */
+  (void) ioctl(STDIN_FILENO, VT_RELDISP, VT_ACKACQ);
+}
+
+static struct vt_mode vtm;
+
 void lock_console_switch(void)
 {
+  struct vt_mode lock_vtm;
+  struct sigaction sa;
+
+  /* Get the virtual console mode. */
+  if (ioctl(STDIN_FILENO, VT_GETMODE, &vtm) < 0) {
+    if (errno == ENOTTY || errno == EINVAL)
+      fprintf(stderr, "vlock-all: this terminal is not a virtual console\n");
+    else
+      perror("vlock-all: could not get virtual console mode");
+
+    return;
+  }
+
+  /* Use a copy of the current virtual console mode. */
+  lock_vtm = vtm;
+
+  (void) sigemptyset(&(sa.sa_mask));
+  sa.sa_flags = SA_RESTART;
+  sa.sa_handler = release_vt;
+  (void) sigaction(SIGUSR1, &sa, NULL);
+  sa.sa_handler = acquire_vt;
+  (void) sigaction(SIGUSR2, &sa, NULL);
+
+  /* set terminal switching to be process governed */
+  lock_vtm.mode = VT_PROCESS;
+  /* set terminal release signal, i.e. sent when switching away */
+  lock_vtm.relsig = SIGUSR1;
+  /* set terminal acquire signal, i.e. sent when switching here */
+  lock_vtm.acqsig = SIGUSR2;
+  /* set terminal free signal, not implemented on either FreeBSD or Linux */
+  /* Linux ignores it but FreeBSD wants a valid signal number here */
+  lock_vtm.frsig = SIGHUP;
+
+  /* set virtual console mode to be process governed
+   * thus disabling console switching */
+  if (ioctl(STDIN_FILENO, VT_SETMODE, &lock_vtm) < 0) {
+    perror("vlock-all: could not set virtual console mode");
+    return;
+  }
+
   console_switch_locked = true;
 }
 
 void unlock_console_switch(void)
 {
+  /* restore virtual console mode */
+  if (ioctl(STDIN_FILENO, VT_SETMODE, vtm) < 0)
+    perror("vlock-all: could not restore console mode");
 }
