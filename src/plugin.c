51c54dd14e978647e5a57478f81424415e6ba275
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-17 23:07:55 +0200

    src/plugin.c: convert to use list_delete_for_each macro

--- src/plugin.c
+++ src/plugin.c
@@ -17,15 +17,14 @@ struct plugin *__allocate_plugin(const char *name)
   return p;
 }
 
-static void free_dependency_list(struct list *dependency_list)
-{
-  list_free(dependency_list, free);
-}
-
 void __destroy_plugin(struct plugin *p)
 {
-  for (size_t i = 0; i < nr_dependencies; i++)
-    list_free(p->dependencies[i], (list_free_item_function)free_dependency_list);
+  for (size_t i = 0; i < nr_dependencies; i++) {
+    list_delete_for_each(p->dependencies[i], dependency_item)
+      free(dependency_item->data);
+
+    list_free(p->dependencies[i]);
+  }
 
   free(p->name);
   free(p);
