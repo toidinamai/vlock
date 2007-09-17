128c702473a9fb427114b477557b3624574d7561
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-17 21:14:40 +0200

    Revert cbad2bd76fee

--- modules/all.c
+++ modules/all.c
@@ -19,59 +19,15 @@
 #include <sys/ioctl.h>
 #include <errno.h>
 #include <signal.h>
-#include <dlfcn.h>
 
 #include "vlock_plugin.h"
+#include "console_switch.h"
 
-struct console_switch_context
-{
-  void *handle;
-  bool *locked;
-  bool (*lock)(char **error);
-  void (*unlock)(void);
-};
-
-struct console_switch_context *load_console_switch(void)
-{
-  struct console_switch_context *console_switch = malloc(sizeof *console_switch);
-
-  if (console_switch == NULL)
-    return NULL;
-
-  console_switch->handle = dlopen(NULL, RTLD_GLOBAL | RTLD_NOW);
-
-
-  if (console_switch->handle == NULL) {
-    fprintf(stderr, "vlock-all: dlopen() failed %s\n", dlerror());
-    free(console_switch);
-    return NULL;
-  }
-
-  console_switch->locked = dlsym(console_switch->handle, "console_switch_locked");
-  *(void **) &(console_switch->lock) = dlsym(console_switch->handle, "lock_console_switch");
-  *(void **) &(console_switch->unlock) = dlsym(console_switch->handle, "unlock_console_switch");
-
-  if (console_switch->lock == NULL || console_switch->unlock == NULL || console_switch->locked == NULL) {
-    fprintf(stderr, "vlock-all: dlsym() failed %s\n", dlerror());
-    dlclose(console_switch->handle);
-    free(console_switch);
-    return NULL;
-  }
-
-  return console_switch;
-}
-
-bool vlock_start(void **ctx_ptr)
+bool vlock_start(void __attribute__((unused)) **ctx_ptr)
 {
   char *error = NULL;
-  struct console_switch_context *console_switch = load_console_switch();
 
-  if (console_switch == NULL) {
-    fprintf(stderr, "vlock-all: could not load console_switch routines from main program\n");
-    return false;
-  }
-
-  if (!console_switch->lock(&error)) {
+  if (!lock_console_switch(&error)) {
     if (error != NULL) {
       fprintf(stderr, "vlock-all: %s\n", error);
       free(error);
@@ -80,19 +36,11 @@ bool vlock_start(void **ctx_ptr)
     }
   }
 
-  *ctx_ptr = console_switch;
-  return *console_switch->locked;
+  return console_switch_locked;
 }
 
-bool vlock_end(void **ctx_ptr)
+bool vlock_end(void __attribute__((unused)) **ctx_ptr)
 {
-  struct console_switch_context *console_switch = *ctx_ptr;
-
-  if (console_switch != NULL) {
-    console_switch->unlock();
-    dlclose(console_switch->handle);
-    free(console_switch);
-  }
-
+  unlock_console_switch();
   return true;
 }
