9d58dc122df5d819cbbd10c152d320dac471fcb5
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-18 20:02:27 +0200

    src/tsort.{c,h}: comments, copyright header

--- src/tsort.h
+++ src/tsort.h
@@ -1,6 +1,17 @@
-#include <stdbool.h>
+/* tsort.h -- header file for topological sort for vlock,
+ *            the VT locking program for linux
+ *
+ * This program is copyright (C) 2007 Frank Benkstein, and is free
+ * software which is freely distributable under the terms of the
+ * GNU General Public License version 2, included as the file COPYING in this
+ * distribution.  It is NOT public domain software, and any
+ * redistribution not permitted by the GNU General Public License is
+ * expressly forbidden without prior written permission from
+ * the author.
+ *
+ */
 
-struct list;
+#include <stdbool.h>
 
 /* An edge of the graph, specifying that predecessor must come before
  * successor. */
@@ -9,7 +20,9 @@ struct edge {
   void *successor;
 };
 
-/* For the given digraph, generate a topological sort of the nodes.
+struct list;
+
+/* For the given directed graph, generate a topological sort of the nodes.
  *
  * Sorts the list and deletes all edges.  If there are circles found in the
  * graph or there are edges that have no corresponding nodes the erroneous
