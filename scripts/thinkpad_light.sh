326cddabac82448977c7146863d001f73351dfcb
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-19 21:27:17 +0200

    scripts/thinkpad_light.sh: don't exit if ThinkLight is intially off

--- scripts/thinkpad_light.sh
+++ scripts/thinkpad_light.sh
@@ -3,19 +3,19 @@
 set -e
 
 hooks() {
-  light_status=$(awk '/^status:/ {print $2}' /proc/acpi/ibm/light)
-
-  if [ "${light_status}" != "on" ] ; then
-    exit
-  fi
-
   while read hook_name ; do
     case "${hook_name}" in
       vlock_save)
-        echo off | sudo tee /proc/acpi/ibm/light >/dev/null
+        light_status=$(awk '/^status:/ {print $2}' /proc/acpi/ibm/light)
+
+        if [ "${light_status}" = "on" ] ; then
+          echo off | sudo tee /proc/acpi/ibm/light >/dev/null
+        fi
       ;;
       vlock_save_abort)
-        echo on | sudo tee /proc/acpi/ibm/light >/dev/null
+        if [ "${light_status}" = "on" ] ; then
+          echo on | sudo tee /proc/acpi/ibm/light >/dev/null
+        fi
       ;;
     esac
   done
