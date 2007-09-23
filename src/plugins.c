66e80da2ac3d1effb3bbc18dde546a4d4f567975
Frank Benkstein <frank-vlock@benkstein.net>
2007-09-23 02:02:37 +0200

    src/plugins.c: use list deletion macro

--- src/plugins.c
+++ src/plugins.c
@@ -83,10 +83,8 @@ void resolve_dependencies(void)
 
 void unload_plugins(void)
 {
-  list_for_each_manual(plugins, plugin_item) {
+  list_delete_for_each(plugins, plugin_item)
     destroy_plugin(plugin_item->data);
-    plugin_item = list_delete_item(plugins, plugin_item);
-  }
 }
 
 void plugin_hook(const char *hook_name)
