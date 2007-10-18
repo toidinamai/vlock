7d8abaa1023883e261c61c05250e64e5bdb10a76
Frank Benkstein <frank-vlock@benkstein.net>
2007-10-18 11:58:05 +0200

    modules/caca.c: remove frame limiting code

--- modules/caca.c
+++ modules/caca.c
@@ -34,7 +34,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <signal.h>
-#include <sys/time.h>
 
 #include <ncurses.h>
 
@@ -77,8 +76,6 @@ void (*fn[])(enum action, cucul_canvas_t *) =
 /* Global variables */
 static int frame = 0;
 static bool abort_requested = false;
-/* The minimum length of one frame.  40ms corresponds to 25 FPS. */
-static struct timeval frame_min_length = {0, 40000};
 
 void handle_sigterm(int __attribute__((unused)) signum)
 {
@@ -161,15 +158,8 @@ static int caca_main(void __attribute__((unused)) *argument)
 
     for(;;)
     {
-        struct timeval frame_start;
-        struct timeval frame_end;
-        struct timeval frame_length;
-
         if (abort_requested)
           goto end;
-
-        /* Remember the start of the frame. */
-        (void) gettimeofday(&frame_start, NULL);
 
         /* Resize the spare canvas, just in case the main one changed */
         cucul_set_canvas_size(backcv, cucul_get_canvas_width(frontcv),
@@ -223,18 +213,6 @@ static int caca_main(void __attribute__((unused)) *argument)
                                    cucul_get_canvas_height(frontcv) - 2,
                                    " -=[ Powered by libcaca ]=- ");
         caca_refresh_display(dp);
-
-        /* Get the current time. */
-        (void) gettimeofday(&frame_end, NULL);
-
-        /* Calculate time elapsed in the current frame. */
-        timersub(&frame_end, &frame_start, &frame_length);
-
-        if (timercmp(&frame_length, &frame_min_length, <)) {
-          /* Calculate the remaining time. */
-          timersub(&frame_min_length, &frame_length, &frame_length);
-          (void) select(0, NULL, NULL, NULL, &frame_length);
-        }
     }
 end:
     if(next != -1)
