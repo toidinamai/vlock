b437f76e76f12364af32cb5717eedb5d7d32938c
Frank Benkstein <frank-vlock@benkstein.net>
2007-12-13 23:50:44 +0100

    tests/test_list.c: add test for list_new

--- tests/test_list.c
+++ tests/test_list.c
@@ -1,7 +1,21 @@
 #include <CUnit/CUnit.h>
 
+#include "list.h"
+
 #include "test_list.h"
 
+void test_list_new(void)
+{
+  struct list *l = list_new();
+
+  CU_ASSERT_PTR_NOT_NULL_FATAL(l);
+  CU_ASSERT_PTR_NULL(l->first);
+  CU_ASSERT_PTR_NULL(l->last);
+
+  list_free(l);
+}
+
 CU_TestInfo list_tests[] = {
+  { "test_list_new", test_list_new },
   CU_TEST_INFO_NULL,
 };
