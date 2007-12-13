6cea6c86b229731f83d482afac1aa6ff88f86340
Frank Benkstein <frank-vlock@benkstein.net>
2007-12-14 00:07:52 +0100

    tests/test_list.c: test_list_copy

--- tests/test_list.c
+++ tests/test_list.c
@@ -15,7 +15,33 @@ void test_list_new(void)
   list_free(l);
 }
 
+void test_list_copy(void)
+{
+  struct list *l = list_new();
+  struct list *m;
+
+  list_append(l, (void *)1);
+  list_append(l, (void *)2);
+  list_append(l, (void *)3);
+
+  m = list_copy(l);
+
+  CU_ASSERT_EQUAL(list_length(l), list_length(m));
+  CU_ASSERT_PTR_NOT_EQUAL(l, m);
+  CU_ASSERT_PTR_NOT_EQUAL(l->first, m->first);
+  CU_ASSERT_PTR_NOT_EQUAL(l->last, m->last);
+
+  for (struct list_item *item_l = l->first, *item_m = m->first;
+      item_l != NULL && item_m != NULL;
+      item_l = item_l->next, item_m = item_m->next)
+    CU_ASSERT_PTR_EQUAL(item_l->data, item_m->data);
+
+  list_free(m);
+  list_free(l);
+}
+
 CU_TestInfo list_tests[] = {
   { "test_list_new", test_list_new },
+  { "test_list_copy", test_list_copy },
   CU_TEST_INFO_NULL,
 };
