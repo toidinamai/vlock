9626aa66f7d35a9a29d00070c1711ac2b7e48cda
Frank Benkstein <frank-vlock@benkstein.net>
2007-12-13 18:54:58 +0100

    tests/vlock-test.c: go through test running stuff

--- tests/vlock-test.c
+++ tests/vlock-test.c
@@ -1,3 +1,28 @@
-int main(void) {
-  return 0;
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+int main(int __attribute__((unused)) argc, const char *argv[])
+{
+  if (CU_initialize_registry() != CUE_SUCCESS) {
+    fprintf(stderr, "%s: CUnit initialization failed\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  if (CU_basic_run_tests() != CUE_SUCCESS) {
+    fprintf(stderr, "%s: running tests failed\n", argv[0]);
+    goto error;
+  }
+
+  if (CU_get_number_of_tests_failed() > 0)
+    goto error;
+
+  CU_cleanup_registry();
+  exit(EXIT_SUCCESS);
+
+error:
+  CU_cleanup_registry();
+  exit(EXIT_FAILURE);
 }
