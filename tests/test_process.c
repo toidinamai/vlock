5e08401a33c868be4b6d07ff2e27d2ffb43d1d5b
Frank Benkstein <frank-vlock@benkstein.net>
2007-12-18 23:56:08 +0100

    tests/test_process.c: test_ensure_death

--- tests/test_process.c
+++ tests/test_process.c
@@ -1,5 +1,8 @@
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <signal.h>
+#include <errno.h>
 
 #include <CUnit/CUnit.h>
 
@@ -21,7 +24,25 @@ void test_wait_for_death(void)
   CU_ASSERT(wait_for_death(pid, 0, 20000));
 }
 
+void test_ensure_death(void)
+{
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    signal(SIGTERM, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+    execl("/bin/true", "/bin/true", NULL);
+    _exit(0);
+  }
+
+  ensure_death(pid);
+
+  CU_ASSERT(waitpid(pid, NULL, WNOHANG) < 0);
+  CU_ASSERT(errno == ECHILD);
+}
+
 CU_TestInfo process_tests[] = {
   { "test_wait_for_death", test_wait_for_death },
+  { "test_ensure_death", test_ensure_death },
   CU_TEST_INFO_NULL,
 };
