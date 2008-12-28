23f51a8ba5c9ee05709d956a6a741db6d6c68bea
Frank Benkstein <frank-vlock@benkstein.net>
2008-12-28 12:19:47 +0100

    src/auth-shadow.c: use GError for error reporting

--- src/auth-shadow.c
+++ src/auth-shadow.c
@@ -24,6 +24,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include <sys/mman.h>
 
@@ -32,7 +33,12 @@
 #include "auth.h"
 #include "prompt.h"
 
-bool auth(const char *user, struct timespec *timeout)
+GQuark vlock_auth_error_quark(void)
+{
+  return g_quark_from_static_string("vlock-auth-shadow-error-quark");
+}
+
+bool auth(const char *user, struct timespec *timeout, GError **error)
 {
   char *pwd;
   char *cryptpw;
@@ -40,40 +46,70 @@ bool auth(const char *user, struct timespec *timeout)
   struct spwd *spw;
   int result = false;
 
-  /* format the prompt */
-  if (asprintf(&msg, "%s's Password: ", user) < 0)
-    return false;
+  g_return_val_if_fail(error == NULL || *error == NULL, false);
 
-  if ((pwd = prompt_echo_off(msg, timeout)) == NULL)
-    goto out_pwd;
+  /* format the prompt */
+  if (asprintf(&msg, "%s's Password: ", user) < 0) {
+    g_propagate_error(error,
+        g_error_new_literal(
+          VLOCK_AUTH_ERROR,
+          VLOCK_AUTH_ERROR_FAILED,
+          g_strerror(errno)));
+    return false;
+  }
+
+  if ((pwd = prompt_echo_off(msg, timeout, error)) == NULL)
+    goto prompt_error;
+
+  errno = 0;
 
   /* get the shadow password */
-  if ((spw = getspnam(user)) == NULL)
-    goto out_shadow;
+  if ((spw = getspnam(user)) == NULL) {
+    if (errno == 0)
+      goto auth_error;
+
+    g_set_error(error,
+        VLOCK_AUTH_ERROR,
+        VLOCK_AUTH_ERROR_FAILED,
+        "Could not get shadow record: %s",
+        g_strerror(errno));
+    goto shadow_error;
+  }
 
   /* hash the password */
   if ((cryptpw = crypt(pwd, spw->sp_pwdp)) == NULL) {
-    perror("vlock: crypt()");
-    goto out_shadow;
+    g_set_error(error,
+        VLOCK_AUTH_ERROR,
+        VLOCK_AUTH_ERROR_FAILED,
+        "crypt() failed: %s",
+        g_strerror(errno));
+    goto shadow_error;
   }
 
   result = (strcmp(cryptpw, spw->sp_pwdp) == 0);
 
   if (!result) {
+auth_error:
     sleep(1);
-    fprintf(stderr, "vlock: Authentication error\n");
+    g_propagate_error(error,
+        g_error_new_literal(
+          VLOCK_AUTH_ERROR,
+          VLOCK_AUTH_ERROR_FAILED,
+          "Authentication failure"));
   }
 
-out_shadow:
+shadow_error:
   /* deallocate shadow resources */
   endspent();
 
   /* free the password */
   free(pwd);
 
-out_pwd:
+prompt_error:
   /* free the prompt */
   free(msg);
+
+  g_assert(result || error != NULL);
 
   return result;
 }
