439ba9d28c339183e1ce8675244c3d70918ce7a5
Frank Benkstein <frank-vlock@benkstein.net>
2009-02-21 09:41:15 +0100

    src/logging.c: simplify logging

--- src/logging.c
+++ src/logging.c
@@ -5,44 +5,20 @@
 
 #include "logging.h"
 
-static void vlock_log_handler(const gchar *log_domain,
-                              GLogLevelFlags __attribute__((unused)) log_level,
-                              const gchar *message,
-                              gpointer __attribute__((unused)) user_data)
+static void vlock_quiet_log_handler(const gchar *log_domain __attribute__((unused)),
+				    GLogLevelFlags log_level __attribute__((unused)),
+				    const gchar *message __attribute__((unused)),
+                                       gpointer user_data __attribute__((unused)))
 {
-  /* XXX: observe the log level (maybe look at some environment variable) */
-  if (log_domain != NULL)
-    fprintf(stderr, "%s(%s): %s\n", g_get_prgname(), log_domain, message);
-  else
-    fprintf(stderr, "%s: %s\n", g_get_prgname(), message);
-}
-
-static void vlock_error_log_handler(const gchar *log_domain,
-                                    GLogLevelFlags log_level,
-                                    const gchar *message,
-                                    gpointer __attribute__((unused)) user_data)
-{
-  vlock_log_handler(log_domain, log_level, message, NULL);
-  abort();
-}
-
-static void vlock_critical_log_handler(const gchar *log_domain,
-                                       GLogLevelFlags log_level,
-                                       const gchar *message,
-                                       gpointer __attribute__(
-                                         (unused)) user_data)
-{
-  vlock_log_handler(log_domain, log_level, message, NULL);
-  exit(1);
+  /* Do nothing ... */
 }
 
 void vlock_initialize_logging(void)
 {
-  g_log_set_handler(NULL, G_LOG_LEVEL_ERROR, vlock_error_log_handler, NULL);
-  g_log_set_handler(NULL,
-                    G_LOG_LEVEL_CRITICAL,
-                    vlock_critical_log_handler,
-                    NULL);
-  g_log_set_default_handler(vlock_log_handler, NULL);
+  const gchar *vlock_debug = g_getenv("VLOCK_DEBUG");
+
+  if (vlock_debug == NULL || *vlock_debug == '\0')
+    g_log_set_handler(NULL, G_LOG_LEVEL_DEBUG | G_LOG_LEVEL_INFO, vlock_quiet_log_handler, NULL);
+
 }
 
